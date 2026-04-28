43a4e96783cbccae67a882c26c107f7c74d20c4d
Dmande6 <143035460+Dmande6@users.noreply.github.com>
2026-04-28 16:53:33 -0500

    Add files via upload

---
+++ finalproject/script.js
@@ -0,0 +1,24 @@
+function openTab(evt, tabName){
+   
+let tabcontent = document.querySelectorAll('.tabcontent');
+let tabs = document.querySelectorAll('.tab');
+
+tabcontent.forEach(section => section.classList.remove('active'));
+tabs.forEach(btn => btn.classList.remove('active'));
+
+document.getElementById(tabName).classList.add('active');
+evt.currentTarget.classList.add('active');
+}
+document.querySelectorAll(".villain").forEach(card => {
+card.addEventListener("click", () => {
+card.classList.toggle("show-desc");
+});
+});
+document.querySelectorAll(".contact").forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("show");
+  });
+});
+
+
+
